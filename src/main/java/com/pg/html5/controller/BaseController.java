3ff89cc08bd9a37ee597c1a50fe6441e7c7543f5
peigen <peigen2011@gmail.com>
2016-02-17 17:14:34 +0900

    server  send msg example

--- src/main/java/com/pg/html5/controller/BaseController.java
+++ src/main/java/com/pg/html5/controller/BaseController.java
@@ -11,5 +11,5 @@ public class BaseController {
 		model.addAttribute("message", "hello world");
 		return "index";
 	}
-
+	
 }
--- src/main/java/com/pg/html5/controller/WebSocketController.java
+++ src/main/java/com/pg/html5/controller/WebSocketController.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pg.html5.bean.Greeting;
 import com.pg.html5.bean.HelloMessage;
@@ -18,5 +19,11 @@ public class WebSocketController {
 	public void greeting(HelloMessage message, @DestinationVariable("chatNo") String chatNo) throws Exception {
 		simpMessagingTemplate.convertAndSend("/topic/chatNo." + chatNo,
 				new Greeting("Hello, " + message.getName() + "!"));
+	}
+
+	@RequestMapping("/sendMsg")
+	public String sendMsg() {
+		simpMessagingTemplate.convertAndSend("/topic/chatNo.1", new Greeting("Hello, " + "message.getName()" + "!"));
+		return "index";
 	}
 }
